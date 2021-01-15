88af3b6e59039318f4368e4a9380cc6e136c4147
niels <niels.post.97@gmail.com>
2021-01-15 17:28:48 +0100

    - Implemented GPL v3

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
+#
+# Copyright (c) 2021. Niels Post. AI Lab Vrije Universiteit Brussel.
+#
+# This file is part of MP-Firm.
+#
+# MP-Firm is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MP-Firm is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MP-Firm.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+
 import setuptools
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
-
-long_description = "yeet"
+long_description = open("README.MD").read()
 
 setuptools.setup(
     name="warehouse_pmsv_tracker",
@@ -13,12 +30,13 @@ setuptools.setup(
     description="/",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://gitlab.ai.vub.ac.be/multi-agent-benchmarking/warehouse-pmsv-tracker",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: Windows 10, Linux",
+        "License :: GPL v3",
+        "Operating System :: Linux"
     ],
+    license=open("COPYING").read(),
     python_requires='>=3.7.3',
 )
--- warehouse_pmsv_tracker/demo/__init__.py
+++ warehouse_pmsv_tracker/demo/__init__.py
@@ -1,9 +1,28 @@
+#
+# Copyright (c) 2021. Niels Post. AI Lab Vrije Universiteit Brussel.
+#
+# This file is part of MP-Firm.
+#
+# MP-Firm is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MP-Firm is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MP-Firm.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+
 from .ArucoDemo import aruco_demo
 from .CameraUndistortionDemo import camera_undistortion_demo
 from .PositionTrackingDemo import position_tracking_demo
 from .PositionTransformerDemo import position_transformer_demo
-from .RobotCommandDemo import robot_command_demo
 from .RobotConnectionDemo import RobotConnectionDemo
 
 
-__all__ = ["aruco_demo", "camera_undistortion_demo", "position_tracking_demo", "position_transformer_demo", "robot_command_demo", "RobotConnectionDemo"]
+__all__ = ["aruco_demo", "camera_undistortion_demo", "position_tracking_demo", "position_transformer_demo", "RobotConnectionDemo"]
